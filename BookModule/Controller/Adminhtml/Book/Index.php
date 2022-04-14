62cdc5e2ccade7f49f6904068d9340818851709b
pret1 <konausiarhei@gmail.com>
2022-04-14 14:56:05 +0300

    create UI admin menu

---
+++ BookModule/Controller/Adminhtml/Book/Index.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ScienceSoft\BookModule\Controller\Adminhtml\Book;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    private PageFactory $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('ScienceSoft_BookModule::menu');
+        $resultPage->getConfig()->getTitle()->prepend(__('Book Menu'));
+        return $resultPage;
+    }
+
+//    /**
+//     * @return bool
+//     */
+//    protected function _isAllowed(): bool
+//    {
+//        return $this->_authorization->isAllowed('ScienceSoft_BookModule::menu');
+//    }
+}
---
+++ BookModule/Controller/Adminhtml/People/User/Phone/Grid.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ScienceSoft\BookModule\Controller\Adminhtml\People\User\Phone;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class Grid extends Action implements HttpGetActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    private PageFactory $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('ScienceSoft_BookModule::menu');
+        $resultPage->getConfig()->getTitle()->prepend(__('Book Menu'));
+        return $resultPage;
+    }
+
+    //    /**
+    //     * @return bool
+    //     */
+    //    protected function _isAllowed(): bool
+    //    {
+    //        return $this->_authorization->isAllowed('ScienceSoft_BookModule::menu');
+    //    }
+}
