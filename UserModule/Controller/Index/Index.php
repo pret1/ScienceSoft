5a8c3ba92ef30a43f0fb3283050f7c7c155be71e
pret1 <konausiarhei@gmail.com>
2022-04-18 22:57:11 +0300

    Data dispaly in frontend

--- UserModule/Controller/Index/Index.php
+++ UserModule/Controller/Index/Index.php
@@ -4,13 +4,44 @@ declare(strict_types=1);
 
 namespace ScienceSoft\UserModule\Controller\Index;
 
-use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+use ScienceSoft\UserModule\Model\ResourceModel\Post\CollectionFactory;
 
-class Index extends Action implements HttpGetActionInterface
+class Index implements HttpGetActionInterface
 {
+    /**
+     * @var PageFactory
+     */
+    private PageFactory $pageFactory;
+
+    /**
+     * @var CollectionFactory
+     */
+    private CollectionFactory $collectionFactory;
+
+    /**
+     * @param PageFactory $pageFactory
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(
+        PageFactory       $pageFactory,
+        CollectionFactory $collectionFactory
+    )
+    {
+        $this->pageFactory = $pageFactory;
+        $this->collectionFactory = $collectionFactory;
+    }
+
     public function execute()
     {
-        echo 'User here';
+        $collection = $this->collectionFactory->create();
+        foreach ($collection as $item) {
+            echo "<pre>";
+            print_r($item->getData());
+            echo "</pre>";
+        }
+
+        return $this->pageFactory->create();
     }
 }
