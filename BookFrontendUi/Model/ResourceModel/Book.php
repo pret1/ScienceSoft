e31df2e338a9ac348636a382c33e9c9c450077ee
pret1 <konausiarhei@gmail.com>
2022-05-04 09:47:30 +0300

    edit Test

--- BookFrontendUi/Model/ResourceModel/Book.php
+++ BookFrontendUi/Model/ResourceModel/Book.php
@@ -7,18 +7,6 @@ use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 class Book extends AbstractDb
 {
     /**
-     * Primary key auto increment flag
-     *
-     * @var bool
-     */
-    protected $_isPkAutoIncrement = false;
-
-    /**
-     * @inheritdoc
-     */
-    protected $_useIsObjectNew = true;
-
-    /**
      * @return void
      * @SuppressWarnings(PHPMD.CamelCaseMethodName)
      */
--- BookFrontendUi/Test/Api/BookRepositoryInterfaceTest.php
+++ BookFrontendUi/Test/Api/BookRepositoryInterfaceTest.php
@@ -5,26 +5,35 @@ declare(strict_types=1);
 namespace ScienceSoft\BookFrontendUi\Test\Api;
 
 use Magento\TestFramework\TestCase\WebapiAbstract;
+use ScienceSoft\BookFrontendUi\Model\ResourceModel\Book\CollectionFactory as BookCollectionFactory;
+use Magento\Framework\Data\Collection\AbstractDb;
 
 class BookRepositoryInterfaceTest extends WebapiAbstract
 {
+    private BookCollectionFactory $bookCollectionFactory;
+
+    public function __construct(
+        BookCollectionFactory $bookCollectionFactory,
+        AbstractDb $abstractDb
+    ) {
+        $this->bookCollectionFactory = $bookCollectionFactory;
+    }
     /**
      * @return void
      * @magentoApiDataFixture Magento/Customer/_files/customer.php
      */
     public function testBookRouting()
     {
-        $regionsCollection = $this->regionsCollectionFactory->create();
-        $regionsCollection->unshiftOrder('region_id', 'ASC');
-        $region = $regionsCollection->getFirstItem()->getId();
-        $itemId = 1000;
+        $bookCollection = $this->bookCollectionFactory->create();
+        $bookCollection->unshiftOrder('region_id', 'ASC');
+        $bookId = $bookCollection->getFirstItem()->getId();
         $serviceInfo = [
             'rest' => [
-                'resourcePath' => '/V1/book_example/' . $itemId,
+                'resourcePath' => '/V1/book_example/' . $bookId,
                 'httpMethod' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_GET,
             ],
         ];
-        $requestData = ['itemId' => $itemId];
+        $requestData = ['itemId' => $bookId];
         $item = $this->_webApiCall($serviceInfo, $requestData);
         $this->assertEquals('egg', $item['name'], "Item was retrieved unsuccessfully");
     }
